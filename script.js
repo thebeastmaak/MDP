c0f8f081e64466f708cce70b803306ba7d849f3b
Soufiane Maakouk <soufianemaakouk@gmail.com>
2025-08-10 19:37:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ function attachChatEvents() {
   input.value = "";
 
   try {
-    const res = await fetch("https://dpp-chatbot-backend.onrender.com/chat", {
+    const res = await fetch("https://chatbot-mdp.onrender.com/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
